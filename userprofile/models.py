a8b4593412d964ab70b7b26343003208c1b0fc8f
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-11-11 23:26:35 +0100

    Add fields to UserProfile

--- userprofile/models.py
+++ userprofile/models.py
@@ -3,7 +3,15 @@ from django.contrib.auth.models import AbstractUser
 
 
 class UserProfile(AbstractUser):
-    email = models.CharField(max_length=40, blank=True, null=True)
+    fullname = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=40, unique=True, blank=True, null=True)
+    phone = models.IntegerField(max_length=15, blank=True)
+    address = models.CharField(max_length=40, blank=True)
+    zipCode = models.IntegerField(max_length=6, blank=True)
+    city = models.CharField(max_length=30, blank=True)
+    country = models.CharField(
+        blank=True, choices=[("da", "Denmark"), ("gb", "Great Britain")]
+    )
 
     def __str__(self):
         return self.user.username
