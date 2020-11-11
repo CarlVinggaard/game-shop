65dc7499ade896ef25e113678bf00f1ecc8e23bf
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-11-11 23:49:26 +0100

    Update user model fields

--- userprofile/models.py
+++ userprofile/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import AbstractUser
 class UserProfile(AbstractUser):
     fullname = models.CharField(max_length=50, blank=True)
     email = models.CharField(max_length=40, unique=True, blank=True, null=True)
-    phone = models.IntegerField(max_length=15, blank=True)
+    phone = models.CharField(max_length=15, blank=True)
     address = models.CharField(max_length=40, blank=True)
-    zipCode = models.IntegerField(max_length=6, blank=True)
+    zipCode = models.CharField(max_length=6, blank=True)
     city = models.CharField(max_length=30, blank=True)
     country = models.CharField(
-        blank=True, choices=[("da", "Denmark"), ("gb", "Great Britain")]
+        max_length=30, blank=True, choices=[("da", "Denmark"), ("gb", "Great Britain")]
     )
 
     def __str__(self):
